872914e3dde2fc3aa18c1461cc8cd7d6d4e5c8cf
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-13 00:11:03 +0300

    Lab6.1.rec

---
+++ AP_Lab№6.1.рек/AP_Lab№6.1.рек.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+void generateArray(int a[], int size, int index = 0) {
+    if (index == size) return;
+    a[index] = rand() % 46 - 10;
+    generateArray(a, size, index + 1);
+}
+
+void printArray(const int a[], int size, int index = 0) {
+    if (index == size) {
+        cout << endl;
+        return;
+    }
+    cout << a[index] << " ";
+    printArray(a, size, index + 1);
+}
+
+int countElements(const int a[], int size, int index = 0) {
+    if (index == size) return 0;
+    return ((a[index] >= 0 || a[index] % 3 != 0)? 1 : 0) + countElements(a, size, index + 1);
+}
+
+int sumElements(const int a[], int size, int index = 0) {
+    if (index == size) return 0;
+    return ((a[index] >= 0 || a[index] % 3 != 0) ? a[index] : 0) + sumElements(a, size, index + 1);
+}
+
+void replaceElements(int a[], int size, int index = 0) {
+    if (index == size) return;
+    if ((a[index] >= 0 || a[index] % 3 != 0)) {
+        a[index] = 0;
+    }
+    replaceElements(a, size, index + 1);
+}
+
+int main() {
+    const int SIZE = 22;
+    srand((unsigned)time(NULL));
+    int a[SIZE];
+
+    generateArray(a, SIZE);
+
+    printArray(a, SIZE);
+
+    cout << "count of elements: " << countElements(a, SIZE) << endl;
+    cout << "sum of elements: " << sumElements(a, SIZE) << endl;
+
+    replaceElements(a, SIZE);
+
+    printArray(a, SIZE);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№6.1.рек/AP_Lab№6.1.рек.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestCountElements)
+        {
+            const int testSize = 5;
+            int testArr[testSize] = { 1, -3, 4, 0, 6 };
+            int count = countElements(testArr, testSize, 0);
+            Assert::AreEqual(4, count);
+        }
+
+        TEST_METHOD(TestSumElements)
+        {
+            const int testSize = 5;
+            int testArr[testSize] = { 1, -3, 4, 0, 6 };
+            int sum = sumElements(testArr, testSize, 0);
+            Assert::AreEqual(11, sum);
+        }
+
+        TEST_METHOD(TestReplaceElements)
+        {
+            const int testSize = 5;
+            int testArr[testSize] = { 1, -3, 4, 0, 6 };
+            replaceElements(testArr, testSize, 0);
+            int expectedArr[testSize] = { 0, -3, 0, 0, 0 };
+            for (int i = 0; i < testSize; i++) {
+                Assert::AreEqual(expectedArr[i], testArr[i]);
+            }
+        }
+    };
+}
